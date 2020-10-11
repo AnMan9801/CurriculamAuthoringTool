c9864da188e219de96062a2d9bef39e909486af1
AnMan9801 <AnMan9801@users.noreply.github.com>
2020-10-11 18:04:46 +0530

    Create Todo component , import in app

---
+++ src/components/Todo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Todo extends Component {
+    render() {
+        return (
+            <div className="container">
+                hi
+            </div>
+        )
+    }
+}
+
+export default Todo
