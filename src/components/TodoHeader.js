8c67f209955456ed55dd80e3d093a30138e1aa0f
AnMan9801 <AnMan9801@users.noreply.github.com>
2020-10-11 23:09:31 +0530

    finalize layout for task with textbox

--- src/components/TodoHeader.js
+++ src/components/TodoHeader.js
@@ -3,7 +3,7 @@ import React from 'react'
 function TodoHeader() {
     return (
         <div className="row border-top border-bottom">
-            <div className="col-2 p-2 pl-0 pr-5 text-capitalize text-wrap text-break">
+            <div className="col-2 pt-2 pb-2 pl-0 pr-5 text-capitalize text-wrap text-break">
                 <span className="font-weight-bolder">Actions</span>
                 <br />
                 <span className="text-secondary">
@@ -11,7 +11,7 @@ function TodoHeader() {
                 </span>
             </div>
             <div className="row col-10 pt-2 pb-2 text-wrap text-break">
-                <div className="col-2"></div>
+                <div className="col-1"></div>
                 <div className="col-10">
                     <span className="font-weight-bolder text-capitaize">Standard</span>
                     <br />
