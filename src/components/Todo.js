b45042b14a5c401eaf6902b21cf2347f871db2c0
AnMan9801 <AnMan9801@users.noreply.github.com>
2020-10-11 20:08:44 +0530

    feat : Add subjectName prop to todo component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 
 export class Todo extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = "indent-0"
+    }
+    
     render() {
         return (
-            <div className="container">
-                hi
+            <div className="container border">
+                <div className="row text-uppercase">{this.props.subjectName}</div>
             </div>
         )
     }
